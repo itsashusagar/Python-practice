3239abc1900d42198852c646217b10351d9c7f80
ashu sagar <itsashusagar@gmail.com>
2023-09-12 10:35:43 +0530

    commit

---
+++ eight.py
@@ -0,0 +1,6 @@
+# Functions
+
+def name(name, age):
+    print(f"my name is {name}. And my age is {age}")
+
+name("Ashu Sagar" , 23)    
---
+++ nineth.py
@@ -0,0 +1,12 @@
+# Arrays And Lists
+
+cars = ["BMW" , "Rolls Royce", "Ferrari", "Jaguar"]
+
+
+print(cars)
+print(len(cars))
+print(type(cars))
+
+# Access List Items
+
+print(cars[0])
